5ce2a05cbe522640dbb9ad1f8bf675d3908a87a4
Anthony <castafolt85@gmail.com>
2023-02-07 15:31:21 +0100

    Bouton suppression

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
 require_once './config/routing.php';
+
+//TODO supression de compte utilisateur via backoffice
+//TODO supression de post via back office
+
+
+//TODO supression de son compte par l'utilisateur
+
+//TODO sécurisation avec HTMLspecialchars sur les controller
+//TODO sécurisation de la session par controller (isConnected)
